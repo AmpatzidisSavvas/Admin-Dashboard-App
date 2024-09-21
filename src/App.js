7ba6ef1fa1247b8471d8c895e6cb5e8951ceab82
AmpatzidisSavvas <savvas.ampa@gmail.com>
2024-09-21 11:29:03 +0300

    Add Route

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ function App() {
               <Route path="/line" element={<Line />} />
               <Route path="/geography" element={<Geography />} />
 
+              <Route path="*" element={<Dashboard />} />
             </Routes>
           </main>
         </div>
